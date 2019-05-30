49cbe1bbd881487372822327bc854b4cf2ddc4ba
Ramon Boss <ramon.boss@outlook.com>
2019-05-30 08:07:01 +0200

    indent

--- code/currsniporig/src/main/scala/code/initial/InitialCode.scala
+++ code/currsniporig/src/main/scala/code/initial/InitialCode.scala
@@ -1,103 +1,102 @@
-package code.initial
+  package code.initial
 
-trait BasicArithmetic[N] {
-  def +(n: N): N
-}
-
-sealed abstract class Number[T <: Number[T]]
-  extends BasicArithmetic[T] {
-  type A = BigInt
-
-  protected def underlying: A
-
-  def toLong: Long = toBigInt.bigInteger.longValueExact()
-  def toBigInt: BigInt = underlying
-
-  def andMask: BigInt
-
-  def apply: A => T
-
-  override def +(num: T): T = apply(checkResult(underlying + num.underlying))
-
-  private def checkResult(result: BigInt): A = {
-    require((result & andMask) == result,
-      "Result was out of bounds, got: " + result)
-    result
-  }
-}
-
-sealed abstract class SignedNumber[T <: Number[T]] extends Number[T]
-
-sealed abstract class Int64 extends SignedNumber[Int64] {
-  override def apply: A => Int64 = Int64(_)
-  override def andMask = 0xffffffffffffffffL
-}
-
-trait BaseNumbers[T] {
-  def zero: T
-  def one: T
-  def min: T
-  def max: T
-}
-
-object Int64 extends BaseNumbers[Int64] {
-  private case class Int64Impl(underlying: BigInt) extends Int64 {
-    require(underlying >= -9223372036854775808L,
-      "Number was too small for a int64, got: " + underlying)
-    require(underlying <= 9223372036854775807L,
-      "Number was too big for a int64, got: " + underlying)
+  trait BasicArithmetic[N] {
+    def +(n: N): N
   }
 
-  lazy val zero = Int64(0)
-  lazy val one = Int64(1)
+  sealed abstract class Number[T <: Number[T]]
+    extends BasicArithmetic[T] {
+    type A = BigInt
 
-  lazy val min = Int64(-9223372036854775808L)
-  lazy val max = Int64(9223372036854775807L)
+    protected def underlying: A
 
-  def apply(long: Long): Int64 = Int64(BigInt(long))
+    def toLong: Long = toBigInt.bigInteger.longValueExact()
+    def toBigInt: BigInt = underlying
 
-  def apply(bigInt: BigInt): Int64 = Int64Impl(bigInt)
-}
+    def andMask: BigInt
+
+    def apply: A => T
+
+    override def +(num: T): T = apply(checkResult(underlying + num.underlying))
+
+    private def checkResult(result: BigInt): A = {
+      require((result & andMask) == result,
+        "Result was out of bounds, got: " + result)
+      result
+    }
+  }
+
+  sealed abstract class SignedNumber[T <: Number[T]] extends Number[T]
+
+  sealed abstract class Int64 extends SignedNumber[Int64] {
+    override def apply: A => Int64 = Int64(_)
+    override def andMask = 0xffffffffffffffffL
+  }
+
+  trait BaseNumbers[T] {
+    def zero: T
+    def one: T
+    def min: T
+    def max: T
+  }
+
+  object Int64 extends BaseNumbers[Int64] {
+    private case class Int64Impl(underlying: BigInt) extends Int64 {
+      require(underlying >= -9223372036854775808L,
+        "Number was too small for a int64, got: " + underlying)
+      require(underlying <= 9223372036854775807L,
+        "Number was too big for a int64, got: " + underlying)
+    }
+
+    lazy val zero = Int64(0)
+    lazy val one = Int64(1)
+
+    lazy val min = Int64(-9223372036854775808L)
+    lazy val max = Int64(9223372036854775807L)
+
+    def apply(long: Long): Int64 = Int64(BigInt(long))
+
+    def apply(bigInt: BigInt): Int64 = Int64Impl(bigInt)
+  }
 
 
-sealed abstract class CurrencyUnit
-  extends BasicArithmetic[CurrencyUnit] {
-  type A
+  sealed abstract class CurrencyUnit
+    extends BasicArithmetic[CurrencyUnit] {
+    type A
 
-  def satoshis: Satoshis
+    def satoshis: Satoshis
 
-  def ==(c: CurrencyUnit): Boolean = satoshis == c.satoshis
+    def ==(c: CurrencyUnit): Boolean = satoshis == c.satoshis
 
-  override def +(c: CurrencyUnit): CurrencyUnit =
-    Satoshis(satoshis.underlying + c.satoshis.underlying)
+    override def +(c: CurrencyUnit): CurrencyUnit =
+      Satoshis(satoshis.underlying + c.satoshis.underlying)
 
-  def toBigDecimal: BigDecimal
+    def toBigDecimal: BigDecimal
 
-  protected def underlying: A
-}
+    protected def underlying: A
+  }
 
-sealed abstract class Satoshis extends CurrencyUnit {
-  override type A = Int64
+  sealed abstract class Satoshis extends CurrencyUnit {
+    override type A = Int64
 
-  override def satoshis: Satoshis = this
+    override def satoshis: Satoshis = this
 
-  override def toBigDecimal = BigDecimal(toBigInt)
+    override def toBigDecimal = BigDecimal(toBigInt)
 
-  def toBigInt: BigInt = BigInt(toLong)
+    def toBigInt: BigInt = BigInt(toLong)
 
-  def toLong: Long = underlying.toLong
+    def toLong: Long = underlying.toLong
 
-  def ==(satoshis: Satoshis): Boolean = underlying == satoshis.underlying
-}
+    def ==(satoshis: Satoshis): Boolean = underlying == satoshis.underlying
+  }
 
-object Satoshis extends BaseNumbers[Satoshis] {
-  val min = Satoshis(Int64.min)
-  val max = Satoshis(Int64.max)
-  val zero = Satoshis(Int64.zero)
-  val one = Satoshis(Int64.one)
+  object Satoshis extends BaseNumbers[Satoshis] {
+    val min = Satoshis(Int64.min)
+    val max = Satoshis(Int64.max)
+    val zero = Satoshis(Int64.zero)
+    val one = Satoshis(Int64.one)
 
-  def apply(int64: Int64): Satoshis = SatoshisImpl(int64)
+    def apply(int64: Int64): Satoshis = SatoshisImpl(int64)
 
-  private case class SatoshisImpl(underlying: Int64) extends Satoshis
-}
-
+    private case class SatoshisImpl(underlying: Int64) extends Satoshis
+  }
