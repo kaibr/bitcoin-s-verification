77a345fb96e3fb8cc9b813bf7e634bb970c246f1
Ramon Boss <ramon.boss@outlook.com>
2019-06-20 08:21:51 +0200

    removed unused comment

--- code/addition/src/main/scala/addition/reduced/number/BasicArithmetic.scala
+++ code/addition/src/main/scala/addition/reduced/number/BasicArithmetic.scala
@@ -1,12 +1,5 @@
 package addition.reduced.number
 
-/**
-  * @define mulSafe
-  * Some classes have restrictions on upper bounds
-  * for it's underlying value. This might cause the `*`
-  * operator to throw. This method wraps it in a `Try`
-  * block.
-  */
 trait BasicArithmetic[N] {
   def +(n: N): N
 }
