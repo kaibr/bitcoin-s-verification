37b61618bdc75c10228cee75775dc5ed10d120e6
Ramon Boss <ramon.boss@outlook.com>
2019-06-28 13:39:12 +0200

    updated script

--- code/addition/bin/stainless.sh
+++ code/addition/bin/stainless.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "" ]; then
+  echo "Missing path parameter. Usage: stainless.sh <path to code>"
+  echo "Taking default value '.'"
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CODE_DIR="modified"
 
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk; PATH=$JAVA_HOME/bin:$PATH; java \
   -jar $DIR/../lib/stainless-0.1.0-93dbd33.jar \
-  $DIR/../src/main/scala/addition/$CODE_DIR/currency/CurrencyUnits.scala \
-  $DIR/../src/main/scala/addition/$CODE_DIR/number/NumberType.scala
+  $(find $1 -type f -name *.scala | tr '\n' ' ')
