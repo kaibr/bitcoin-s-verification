6525b56e2a1d1a4b8c84a1b44fcc67d90cb2ee64
kaibr kai.bruennler@gmail.com <kai.bruennler@gmail.com>
2019-06-28 16:12:58 +0200

    remove static JAVA_HOME

--- code/bin/stainless.sh
+++ code/bin/stainless.sh
@@ -8,6 +8,6 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CODE_DIR="modified"
 
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk; PATH=$JAVA_HOME/bin:$PATH; java \
+PATH=$JAVA_HOME/bin:$PATH; java \
   -jar $DIR/../lib/stainless-0.1.0-93dbd33.jar \
   $(find $1 -type f -name *.scala | tr '\n' ' ')
