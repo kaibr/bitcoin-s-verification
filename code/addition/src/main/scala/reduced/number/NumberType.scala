37e36db5dbf2622c4e44a5f2844ebd4f9633002d
kaibr kai.bruennler@gmail.com <kai.bruennler@gmail.com>
2019-06-29 17:00:01 +0200

    remove comments

--- code/addition/src/main/scala/reduced/number/NumberType.scala
+++ code/addition/src/main/scala/reduced/number/NumberType.scala
@@ -1,35 +1,14 @@
 package reduced.number
 
-/**
-  * This abstract class is meant to represent a signed and unsigned number in C
-  * This is useful for dealing with codebases/protocols that rely on C's
-  * unsigned integer types
-  */
 sealed abstract class Number[T <: Number[T]] {
   type A = BigInt
-
-  /** The underlying scala number used to to hold the number */
   protected def underlying: A
-
   def toLong: Long = toBigInt.bigInteger.longValueExact()
   def toBigInt: BigInt = underlying
-
-  /**
-    * This is used to determine the valid amount of bytes in a number
-    * for instance a UInt8 has an andMask of 0xff
-    * a UInt32 has an andMask of 0xffffffff
-    */
   def andMask: BigInt
-
-  /** Factory function to create the underlying T, for instance a UInt32 */
   def apply: A => T
-
   def +(num: T): T = apply(checkResult(underlying + num.underlying))
 
-  /**
-    * Checks if the given result is within the range
-    * of this number type
-    */
   private def checkResult(result: BigInt): A = {
     require((result & andMask) == result,
       "Result was out of bounds, got: " + result)
@@ -37,24 +16,13 @@ sealed abstract class Number[T <: Number[T]] {
   }
 }
 
-/**
-  * Represents a signed number in our number system
-  * Instances of this is [[Int64]]
-  */
 sealed abstract class SignedNumber[T <: Number[T]] extends Number[T]
 
-/**
-  * Represents a int64_t in C
-  */
 sealed abstract class Int64 extends SignedNumber[Int64] {
   override def apply: A => Int64 = Int64(_)
   override def andMask = 0xffffffffffffffffL
 }
 
-/**
-  * Represents various numbers that should be implemented
-  * inside of any companion object for a number
-  */
 trait BaseNumbers[T] {
   def zero: T
   def one: T
@@ -72,11 +40,9 @@ object Int64 extends BaseNumbers[Int64] {
 
   lazy val zero = Int64(0)
   lazy val one = Int64(1)
-
   lazy val min = Int64(-9223372036854775808L)
   lazy val max = Int64(9223372036854775807L)
 
   def apply(long: Long): Int64 = Int64(BigInt(long))
-
   def apply(bigInt: BigInt): Int64 = Int64Impl(bigInt)
 }
